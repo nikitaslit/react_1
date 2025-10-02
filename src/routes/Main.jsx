900bb99bf42302151be3dc688a2916792e7207b9
nikitaslit <nikitaslit@mail.ru>
2025-10-02 21:54:32 +0500

    first commit

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -7,11 +7,20 @@ import InstructionCard from "../components/InstructionCard";
 const MainPage = () => {
   const navigate = useNavigate();
   const params = useParams();
+  const [login,setLogin] = useState("Admin");
+  const [password, setPassword] = useState("Admin")
+
+  const [showLogin,setShowLogin]=useState("");
+  const [showPassword,setShowPassword]=useState("");
 
   const texts = useMemo(
-    () => ["hello world", "привет мир", "здравствуй, реальность"],
+    () => ["чего вы больше всего боитесь в программированиии", "привет мир", "здравствуй, реальность"],
     []
   );
+  const sendData= () =>{
+    setShowLogin(login);
+    setShowPassword(password);
+  }
 
   const [textHello, setTextHello] = useState(texts[0]);
 
@@ -27,6 +36,13 @@ const MainPage = () => {
     <>
       <div className="main">
         <div className="main__container">
+          <div>
+            <div>Твои данные</div>
+            <div>{showLogin}/{showPassword}</div> 
+            <input value={login} onChange={(e) => setLogin(e.target.value)}></input>
+            <input value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <div><button onClick={()=>{sendData()}} className="btn">отправить </button></div>
+          </div>
           <HelloCard text={textHello} onNext={handleNext} />
           <InstructionCard />
         </div>
